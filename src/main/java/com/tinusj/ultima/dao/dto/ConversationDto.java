e529c76cb66af7ce0fdb442af79187dadc6c69c9
Tinus <jacksontinus96@gmail.com>
2025-04-18 08:39:47 +0200

    Initial chat functionality

---
+++ src/main/java/com/tinusj/ultima/dao/dto/ConversationDto.java
@@ -0,0 +1,11 @@
+package com.tinusj.ultima.dao.dto;
+
+import java.time.LocalDateTime;
+
+public record ConversationDto(
+        Long id,
+        Long otherParticipantId,
+        String otherParticipantUsername,
+        String lastMessage,
+        LocalDateTime lastMessageAt
+) {}
---
+++ src/main/java/com/tinusj/ultima/dao/dto/UserSearchDto.java
@@ -0,0 +1,6 @@
+package com.tinusj.ultima.dao.dto;
+
+public record UserSearchDto(
+        Long id,
+        String username
+) {}
