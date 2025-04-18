192a535e598275c771ca5d493ddf48b7ca0a225f
Tinus <jacksontinus96@gmail.com>
2025-04-18 17:49:13 +0200

    Reordered classes to match functionality

--- src/main/java/com/tinusj/ultima/config/OpenApiConfig.java
+++ src/main/java/com/tinusj/ultima/config/OpenApiConfig.java
@@ -1,12 +1,17 @@
 package com.tinusj.ultima.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
+@OpenAPIDefinition(servers = {@Server(url = "/",
+        description = "any description of Server URL")
+})
 public class OpenApiConfig {
 
     @Bean
@@ -15,13 +20,6 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("Ultima API")
                         .version("0.0.1")
-                        .description("API for managing features in the Ultima PrimeNG theme"))
-                .addSecurityItem(new io.swagger.v3.oas.models.security.SecurityRequirement().addList("bearerAuth"))
-                .components(new io.swagger.v3.oas.models.Components()
-                        .addSecuritySchemes("bearerAuth",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")));
+                        .description("API for managing features in the Ultima PrimeNG theme"));
     }
 }
