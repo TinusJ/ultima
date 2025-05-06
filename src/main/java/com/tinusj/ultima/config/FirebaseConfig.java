b28fef941f215450ff9c717cb1db6ca018182be4
Tinus <jacksontinus96@gmail.com>
2025-05-06 18:35:09 +0200

    A

--- src/main/java/com/tinusj/ultima/config/FirebaseConfig.java
+++ src/main/java/com/tinusj/ultima/config/FirebaseConfig.java
@@ -7,12 +7,14 @@ import com.google.firebase.auth.FirebaseAuth;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
 
 @Slf4j
 @Configuration
+@Profile("firebase")
 public class FirebaseConfig {
 
     @Bean
